31a2c41b107398e986d599c49e8f6270a4a65c9a
KHIRDDINE Lois <lois.khirddine@etu.univ-amu.fr>
2026-04-13 19:06:12 +0200

    Upload New File

---
+++ Instances_data/Non-rand_data/SQL_FILES/non_rand_pure_2022.sql
@@ -0,0 +1,7 @@
+SELECT hash,nb_vars,"verified-result",family,track FROM "results_non-rand_2022" WHERE
+"verified-result"="sat" AND
+NOT(track like "%main_2024%" OR track like "%main_2023%")
+AND
+NOT (family like "%random%")
+ORDER BY(CAST(nb_vars AS INTEGER));
+---CAST(nb_vars AS INTEGER)<=2000;---
